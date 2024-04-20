7e07c51b94b8f96e65e025c4e05109076f7f7e19
tanadondev <142754243+tanadonhundev@users.noreply.github.com>
2024-04-20 08:30:52 +0700

    add socket

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ const report = require("./routes/report");
 const chat = require("./routes/chat");
 const message = require("./routes/message");
 
+const io = require('./socketServer');
+
 connectDB();
 
 // Middlewares
