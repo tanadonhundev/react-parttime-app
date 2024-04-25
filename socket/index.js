5f67453a1750e07b48fbf37948a45d5ff17e3128
tanadondev <142754243+tanadonhundev@users.noreply.github.com>
2024-04-26 00:11:55 +0700

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -1,12 +1,23 @@
-const { Server } = require("socket.io")
+const { Server } = require("socket.io");
+const express = require("express");
 const cors = require("cors");
 
 require('dotenv').config();
 const url = process.env.CLIENT_URL;
 
-const io = new Server({ cors: { origin: url } })
+const app = express();
+app.use(cors({
+    origin: url, // Allow requests from the specified origin
+    methods: ["GET", "POST"], // Allow only GET and POST requests
+}));
 
-let onlineUsers = []
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${server.address().port}`);
+});
+
+const io = new Server(server);
+
+let onlineUsers = [];
 
 io.on("connection", (socket) => {
     //console.log("New Connection", socket.id)
@@ -18,25 +29,21 @@ io.on("connection", (socket) => {
                 socketId: socket.id,
             });
         //console.log("onlineUsers", onlineUsers)
-        io.emit("getOnlineUsers", onlineUsers)
+        io.emit("getOnlineUsers", onlineUsers);
     });
 
     // add message
     socket.on("sendMessage", (message) => {
-        const user = onlineUsers.find(user => user.userId === message.receiverId)
+        const user = onlineUsers.find(user => user.userId === message.receiverId);
         //console.log(message.receiverId)
         if (user) {
             //console.log(user)
-            io.to(user.socketId).emit("getMessage", message
-            )
+            io.to(user.socketId).emit("getMessage", message);
         }
-    })
+    });
 
     socket.on("disconnect", () => {
-        onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id)
-        io.emit("getOnlineUsers", onlineUsers)
+        onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id);
+        io.emit("getOnlineUsers", onlineUsers);
     });
 });
-const port = process.env.PORT || 3000;
-
-io.listen(port);
